fff7b5b52934f83786847479d88e8c3098e3b065
Qiluo <lvvote@gmail.com>
2023-06-25 21:06:14 +0800

    添加图片显示

---
+++ Tkinter/display_widget/image.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+# 创建 tkinter 窗口
+root = tk.Tk()
+
+# 加载图片
+image = Image.open("pattern.png")
+photo = ImageTk.PhotoImage(image)
+
+# 创建 Label，并设置图片
+label = tk.Label(root, image=photo)
+label.pack()
+
+# 运行窗口主循环
+root.mainloop()
